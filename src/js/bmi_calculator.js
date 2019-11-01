a08e1b7cca87a0c4cd78bb230dba8897a17804c8
Johan <sundqvist.johan@outlook.com>
2019-11-01 11:27:25 +0100

    Add code to pass BMI value test

--- src/js/bmi_calculator.js
+++ src/js/bmi_calculator.js
@@ -1,3 +1,14 @@
 function BMICalculator() {
-    
+    BMICalculator.prototype.metric_bmi = function(obj) {
+        let weight = obj.weight;
+        let height = obj.height;
+        if (weight > 0 && height > 0) {
+            var finalBmi = weight / (height / 100 * height / 100);
+            obj.bmiValue = parseFloat(finalBmi.toFixed(2));
+        }
+    };
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = BMICalculator;
 }
