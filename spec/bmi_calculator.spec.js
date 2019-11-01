b07e547a484510b2e81ae5b3086a1c2cee9e8510
Johan <sundqvist.johan@outlook.com>
2019-11-01 11:36:27 +0100

    Add BMI message

--- spec/bmi_calculator.spec.js
+++ spec/bmi_calculator.spec.js
@@ -5,12 +5,17 @@ describe('BMICalculator', () => {
     let person
 
     beforeEach(() => {
-        person = new person({weight: 90, height: 186});
+        person = new Person({weight: 90, height: 186});
         bmi_calculator = new BMICalculator();
     });
 
     it('calculates BMI for a person using metric method', () => {
         bmi_calculator.metric_bmi(person)
         expect(person.bmiValue).to.eql(26.01)
-    })
+    });
+
+    it('sets BMI message for a person using metric method', () => {
+        bmi_calculator.metric_bmi(person);
+        expect(person.bmiMessage).to.equal('Overweight');
+    });
 })
